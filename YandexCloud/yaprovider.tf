f81a9b92927637d9028859e6c528d59859f9c1a6
Vladimir <dreaddevil@mail.ru>
2024-01-26 23:56:05 +0300

    test

---
+++ YandexCloud/yaprovider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+  backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "tf-state-bucket-mentor"
+    region     = "ru-central1-a"
+    key        = "issue1/lemp.tfstate"
+    access_key = "YCAJEPr6lZRlHoXG8Ulk4eqAW"
+    secret_key = "YCOpSp002-T1I2NsvonwSRMtnvGbUhI2yPxFFjr4"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+provider "yandex" {
+  token     = "y0_AgAAAABmOtwFAATuwQAAAADVYJG9wgJFmqsESdCTNX0QRhiCkansvqA"
+  cloud_id  = "<b1g0utbakijalqm7pqve"
+  folder_id = "b1g4oh4ltrfd2mnlravq"
+  zone      = "ru-central1-a"
+}
---
+++ YandexCloud/yc/.terraform.lock.hcl
@@ -0,0 +1,11 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version = "0.89.0"
+  hashes = [
+    "h1:NqCwLVdLit67VPSvU1GyQKgz2oB7lyt/sVD3HyY14vs=",
+    "h1:Qq7hBdRVatAaNwmf/xnr9g84PbCj+InH9oiZQgIywyY=",
+    "h1:xVJHMDz3dJjX71KVeztLuRFTpfpn5pUIBfFj+DkyLHc=",
+  ]
+}
